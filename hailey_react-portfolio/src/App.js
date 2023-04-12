751ef47f8729e46024915a02feff6b67d8e39e21
Hailey Francis <n01402670@unf.edu>
2023-04-12 15:18:30 -0400

    added pikachu

---
+++ hailey_react-portfolio/src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import PageContainer from "./components/PageContainer";
+import "./styles/Background.css"
+
+const App = () => <PageContainer />;
+
+export default App;
---
+++ hailey_react-portfolio/src/components/PageContainer.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import AboutMe from "./pages/AboutMe"
+import ContactMe from './pages/ContactMe';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+import Pikachu from '../images/pikachu-run.gif'
+
+import "../styles/PikachuScroll.css"
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('About Me');
+
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+    if(currentPage === 'About Me'){
+      return <><AboutMe /><Footer /></>
+    }
+    if(currentPage === 'Portfolio'){
+      return <><Portfolio/><Footer /></>
+    }
+    if(currentPage === "Contact Me"){
+      return <><ContactMe /><Footer /></>
+    }
+    if(currentPage === "Resume"){
+      return <><Resume /><Footer /></>
+    }
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <>
+      <div class="scroll-container">
+        <div class="sliding-background"></div>
+      </div>
+      <h1 class="hailey-title">Hailey D. Francis</h1>
+      <img src={Pikachu} class="pikachu" alt="running Pikachu"></img>
+      <header class="header-container">
+        <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+      </header>
+      
+      <main>
+        {renderPage()}
+      </main>
+    </>
+    
+  );
+}

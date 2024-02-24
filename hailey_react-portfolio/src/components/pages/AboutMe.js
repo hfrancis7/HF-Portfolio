eff04e13b07b0ae3db8ff7c2d77cb5412f706ad0
Hailey Francis <73856469+hfrancis7@users.noreply.github.com>
2024-02-24 15:51:38 -0500

    portfolio about me page update

--- hailey_react-portfolio/src/components/pages/AboutMe.js
+++ hailey_react-portfolio/src/components/pages/AboutMe.js
@@ -8,10 +8,10 @@ export default function AboutMe() {
     <div class="aboutme">
       <h1 class="heading">About Me</h1>
       <img src={profilePic} class="profileImg" alt="Hailey Francis Profile Pic"></img>
-      <p>I graduated in Spring 2022 from the University of North Florida (UNF) with a degree in Computer Science and a minor in Mathematics. I've also recently graduated the University of Central Florida's (UCF) October 2022 24-week Web Development bootcamp in May 2023. I went through this program to gain better professional skillset to succeed in the workforce.</p> 
-      <p>I am a quick learner with a strong attention to detail. My most recent experience has been with HTML,CSS, and Javascript, especially with Node.js. I have experience with the full MERNG stack, as well as mySQL and designing relational databases. I also have experience working with Java, Python, and C, with exposure to computing concepts such as data structures, networking practices, and AI.</p>
-      <p>I'm confident that my education through college, my computing-related hobbies, and and UCF's Bootcamp program will help me open doors to jobs in web development and software development.</p>
-      <p>This website is a single-page website built with REACT, and utilizes conditional rendering to switch between components based on what's selected in the Navigation Bar.</p>
+      <p>Hi, I'm Hailey! I'm passionate about developing solutions using my knowledge of development methodoligies such as AGILE. I have experience operating in both a classic web development environment as well as ServiceNow's platform environment. </p> 
+      <p>I have a strong attention to detail, and love exploring every avenue to find the best possible solutions. I also love being able to create solutions that have the ability to help people achieve what they want to accomplish: if I'm able to do that, then I feel accomplished, too.</p>
+      <p>I have the most experience with JavaScript, using technologies such as REACT, Bootstrap, Node, Express, and Angular. These are just a handful of examples of the types of technologies I've used before, and I'm always learning more. </p>
+      <p>I'm also a child at heart, as you can see from my portfolio's color scheme and the Pikachu on top of the page. The ability to show who I am through this page is another feature I find empowering in development, and it gives me the confidence and experience in what I'm capable of creating.</p>
     </div>
   )
 }
